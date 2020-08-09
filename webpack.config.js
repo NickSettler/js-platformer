e1a67bfff783e9e3c5550f269e4e00d660c4d90a
Nick Settler <moiseevnikita14@gmail.com>
2020-08-09 18:13:05 +0300

    (config) changed webpack devserver port

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
 	],
 	devServer: {
 		host: '0.0.0.0',
-		port: 8081,
+		port: 8082,
 		contentBase: path.resolve(__dirname, 'dist'),
 		disableHostCheck: true,
 		compress: true,
