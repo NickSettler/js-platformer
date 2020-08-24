b1993cf6dd602f4480c219847827f9d9793ec47a
NickSettler <moiseevnikita14@gmail.com>
2020-08-24 13:52:06 +0500

    (code) created main script

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Game from './Game';
+import Scene from "./Scene";
+import {globalErrorHandler} from "./functions";
+import Rect from "./Rect";
+import {COLORS} from "./Colors";
+
+window.onerror = globalErrorHandler;
+
+const game = new Game(
+    document.getElementById("main_canvas") as HTMLCanvasElement,
+    {
+        width: 800,
+        height: 600
+    }
+);
+
+const s1 = new Scene({
+    name: "Main Scene",
+    isMain: true
+});
+
+// for (let i = 0; i <= 100; i++) {
+//     for (let j = 0; j < 75; j++) {
+//         s1.addEntity(new Rect({
+//             x: i * 8, y: j * 8, w: 8, h: 8, isColorRandomized: true
+//         }));
+//     }
+// }
+
+const r1 = new Rect({
+    x: 100, y: 100,
+    w: 20, h: 30,
+});
+
+// @ts-ignore
+r1.color = COLORS.RED;
+
+setTimeout(() => {
+    r1.resize(100, 100);
+}, 1000);
+
+s1.addEntity(r1);
+
+game.addScene(s1);
+window.onload = () => {
+    game.startProcess();
+};
+
+
