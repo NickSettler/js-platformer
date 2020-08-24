35605e21c1a0dce8b9927e3bdc7f5548e170d5a4
NickSettler <moiseevnikita14@gmail.com>
2020-08-24 13:51:01 +0500

    (code) created ColorfulEntity class

---
+++ src/ColorfulEntity.ts
@@ -0,0 +1,41 @@
+import {COLORS} from "./Colors";
+import {EntityOptions} from "./Entity";
+
+interface ColorfulEntityInterface {
+
+}
+
+type ColorfulEntityOptions = {
+    color?: COLORS | string;
+    isColorRandomized?: boolean;
+}
+
+const DefaultColorfulEntityOptions: ColorfulEntityOptions = {
+    isColorRandomized: false,
+};
+
+export default class ColorfulEntity {
+    protected _color: string;
+    protected _isColorRandomized: boolean;
+
+    constructor(options: ColorfulEntityOptions = DefaultColorfulEntityOptions) {
+        this._color = options.color;
+        this._isColorRandomized = options.isColorRandomized;
+    }
+
+    set color(color: COLORS | string) {
+        this._color = color;
+    }
+
+    get color(): string {
+        return this._color;
+    }
+
+    set isColorRandomized(isRandom: boolean){
+        this._isColorRandomized = isRandom;
+    }
+
+    get isColorRandomized(): boolean {
+        return this._isColorRandomized;
+    }
+}
