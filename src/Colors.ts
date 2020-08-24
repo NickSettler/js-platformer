eae3246b86109e5d26a6e35811d9c8a93b588310
NickSettler <moiseevnikita14@gmail.com>
2020-08-24 13:47:53 +0500

    (code) created COLORS enum

---
+++ src/Colors.ts
@@ -0,0 +1,10 @@
+export enum COLORS {
+    BLACK = `rgba(0, 0, 0, 1)`,
+    BLUE = `rgba(0, 0, 255, 1)`,
+    GREEN = `rgba(0, 255, 0, 1)`,
+    CYAN = `rgba(0, 255, 255, 1)`,
+    RED = `rgba(255, 0, 0, 1)`,
+    PINK = `rgba(255, 0, 255, 1)`,
+    YELLOW = `rgba(255, 255, 0, 1)`,
+    WHITE = `rgba(255, 255, 255, 1)`,
+}
